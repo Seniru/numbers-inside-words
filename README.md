6d1ddddbdac1b07c130642a10652d386c8965922
TheChickenNagget <chickennagget14@gmail.com>
2019-09-25 20:19:52 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Numbers inside Words
+## Module to do number words like M1n3cr4ft
+----
+## Usage
+```js 
+var package = require("numbers-inside-words");
+package.create("minecraft");
+// outputs m1n3cr4ft
+```
---
+++ core.js
@@ -0,0 +1,28 @@
+"use strict";
+require("v8-compile-cache");
+module.exports = {
+create:function(value){
+if(typeof value === "number" || "boolean" || "object"){throw new Error("Text must be string");};
+
+var i;
+var rslSize1 = value.replace(/[^a]/g, "").length;
+var rslSize2 = value.replace(/[^A]/g, "").length;
+var rslSize3 = value.replace(/[^o]/g, "").length;
+var rslSize4 = value.replace(/[^O]/g, "").length;
+var rslSize5 = value.replace(/[^e]/g, "").length;
+var rslSize6 = value.replace(/[^E]/g, "").length;
+var rslSize7 = value.replace(/[^i]/g, "").length;
+var rslSize8 = value.replace(/[^I]/g, "").length;
+
+for (i = 0; i < rslSize3; i++){value = value.replace("o", "0");};
+for (i = 0; i < rslSize4; i++){value = value.replace("O", "0");};
+for (i = 0; i < rslSize2; i++){value = value.replace("A", "4");};
+for (i = 0; i < rslSize1; i++){value = value.replace("a", "4");};
+for (i = 0; i < rslSize5; i++){value = value.replace("e", "3");};
+for (i = 0; i < rslSize6; i++){value = value.replace("E", "3");};
+for (i = 0; i < rslSize6; i++){value = value.replace("E", "3");};
+for (i = 0; i < rslSize7; i++){value = value.replace("i", "1");};
+for (i = 0; i < rslSize8; i++){value = value.replace("I", "1");};
+return value.toString()
+    }
+};
