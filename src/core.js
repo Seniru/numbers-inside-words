79ea24bc3f607cbd7aff67aef0d87952adf65fe4
TheChickenNagget <thechickennagget@gmail.com>
2019-09-29 10:55:53 +0200

    update core

--- src/core.js
+++ src/core.js
@@ -1,23 +1,13 @@
 "use strict";
-
 require("v8-compile-cache");
-
 const dict = {
-    "o": "0",
-    "O": "0",
-    "a": "4",
-    "A": "4",
-    "e": "3",
-    "E": "3",
-    "i": "1",
-    "I": "1"
-}
+ "o": "0", "O": "0", "a": "4", "A": "4",
+ "e": "3", "E": "3", "i": "1", "I": "1"
+};
 
 module.exports = {
-    create: function(value) {
-        for (let k of Object.keys(dict)) {
-            value = value.replace(new RegExp(k, "g"), dict[k]);
-        }
-        return value;
-    }
-};
+create: function(val) {
+for (let k of Object.keys(dict)){ 
+val = val.replace(new RegExp(k, "g"), dict[k]); }
+     return val;
+}};
