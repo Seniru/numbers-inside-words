1cf3067b6e123ab5d012fe9b86e350abdf5faa53
Seniru Pasan Indira <senirupasan@gmail.com>
2019-10-05 07:10:22 +0000

    Modified test cases
    Modified the test cases so it does align with the changes to the core

--- test/test.js
+++ test/test.js
@@ -2,22 +2,23 @@ const assert = require("assert");
 const nw = require("../src/core");
 
 describe("Numbers Inside Words", () => {
-    describe("#create()", () => {
-        it("Should convert words", () => {
-            assert.equal(nw.create("minecraft"), "m1n3cr4ft");
-            assert.equal(nw.create("oOaAeEiI"), "00443311");
-            assert.equal(nw.create("InSide"), "1nS1d3");
+
+    it("Should convert words", () => {
+        assert.equal(nw("minecraft"), "m1n3cr4ft");
+        assert.equal(nw("oOaAeEiI"), "00443311");
+        assert.equal(nw("InSide"), "1nS1d3");
         });
-        it("Should convert sentences", () => {
-            assert.equal(nw.create("Chicken Nuggets are Tasty!"), "Ch1ck3n Nugg3ts 4r3 T4sty!");
-            assert.equal(nw.create("Lorem  Ipsum"), "L0r3m  1psum");
-            assert.equal(nw.create("Order from Chaos"), "0rd3r fr0m Ch40s");
-        });
-        it("Shouldn't convert other symbols and letters", () => {
-            assert.equal(nw.create("bcdfghjkl"), "bcdfghjkl");
-            assert.equal(nw.create("~!@#$%^&*()_+|"), "~!@#$%^&*()_+|");
-            assert.equal(nw.create("    "), "    ");
-        })
+
+    it("Should convert sentences", () => {
+        assert.equal(nw("Chicken Nuggets are Tasty!"), "Ch1ck3n Nugg3ts 4r3 T4sty!");
+        assert.equal(nw("Lorem  Ipsum"), "L0r3m  1psum");
+        assert.equal(nw("Order from Chaos"), "0rd3r fr0m Ch40s");
+    });
+
+    it("Shouldn't convert other symbols and letters", () => {
+        assert.equal(nw("bcdfghjkl"), "bcdfghjkl");
+        assert.equal(nw("~!@#$%^&*()_+|"), "~!@#$%^&*()_+|");
+        assert.equal(nw("    "), "    ");
     });
 });
 
